273687e9a493a20b7dc9216950807c54bb54e8f3
Edjery <edisgumbao@gmail.com>
2024-08-13 11:40:22 +0800

    Utilize CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
 import router from "./router.tsx";
 import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
