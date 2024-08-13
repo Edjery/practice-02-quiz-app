bea88ff66c7b6eb60e47f9aa40f27e81e9f8aa99
Edjery <edisgumbao@gmail.com>
2024-08-13 13:27:50 +0800

    utilize react spring to animate home page

--- src/component/home/List.tsx
+++ src/component/home/List.tsx
@@ -1,16 +1,39 @@
 import { Grid } from "@mui/material";
+import { animated, config, useSpring } from "@react-spring/web";
+import { useEffect, useState } from "react";
 import QuestionSetList from "../../values/questionSet/QuestionSetList";
 import QuizCard from "./QuizCard";
 
 const List = () => {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
   const quizList = QuestionSetList;
+
   return (
     <Grid container spacing={1}>
-      {quizList.map((quiz, index) => (
-        <Grid key={index} item xs={4}>
-          <QuizCard questionSet={quiz} index={index} />
-        </Grid>
-      ))}
+      {quizList.map((quiz, index) => {
+        const props = useSpring({
+          to: {
+            opacity: animate ? 1 : 0,
+            transform: animate ? "translateY(0)" : "translateY(100px)",
+          },
+          from: { opacity: 0, transform: "translateY(100px)" },
+          config: config.slow,
+          delay: index * 50,
+        });
+
+        return (
+          <Grid key={index} item xs={4}>
+            <animated.div style={props}>
+              <QuizCard questionSet={quiz} index={index} />
+            </animated.div>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
