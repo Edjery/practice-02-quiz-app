82f0ee09b3aa8fc37e52c638ab51980c474c34c4
Edjery <edisgumbao@gmail.com>
2024-08-14 11:35:32 +0800

    item not in use

--- src/component/home/ListSkeleton.tsx
+++ src/component/home/ListSkeleton.tsx
@@ -7,7 +7,7 @@ interface Props {
 const ListSkeleton = ({ list }: Props) => {
   return (
     <Grid container spacing={4}>
-      {list.map((item, index) => {
+      {list.map((_, index) => {
         return (
           <Grid key={index} item xs={12} sm={6} md={4}>
             <Skeleton variant="rectangular" animation="wave" sx={{ height: "250px", width: "auto", borderRadius: "10px" }} />
